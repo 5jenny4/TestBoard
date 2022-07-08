d13069ef6fb315b57c1b76399761571557b39c0a
5jenny4 <cloudctworld@gmail.com>
2022-07-08 18:04:24 +0900

    2022-07-08 csy
    뷰 연습용 게시판

---
+++ testboard-back/testboard-back/src/main/java/com/example/testboardback/TestboardBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.testboardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestboardBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestboardBackApplication.class, args);
+	}
+
+}
---
+++ testboard-back/testboard-back/src/test/java/com/example/testboardback/TestboardBackApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.testboardback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+class TestboardBackApplicationTests {
+	public static void main(String[] args){
+		SpringApplication.run(TestboardBackApplication.class, args);
+	}
+
+}
